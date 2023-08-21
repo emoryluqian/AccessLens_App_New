049edac91f82052debad75ea929ebae7ae21f3f2
Emory Lu <emoryluqian@gmail.com>
2023-08-21 11:48:03 -0500

    Add restart panel

--- Assets/Scripts/Manager_ResultScene.cs
+++ Assets/Scripts/Manager_ResultScene.cs
@@ -1,9 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager_ResultScene : MonoBehaviour
 {
+    public GameObject Panel_Summary;    // Summary View
+    public GameObject Panel_Suggestion; // Suggestion View
+    public GameObject Panel_3DPrint;    // Detailed 3D-printed Augmentation Object View 
+    public GameObject Panel_Restart;
+
+    #region Panel_Restart
+
+    public void EnableRestartPanel()
+    {
+        Panel_Restart.SetActive(true);
+    }
+
+    public void DisableRestartPanel()
+    {
+        Panel_Restart.SetActive(false);
+    }
+
+    public void ConfirmRestart()
+    {
+        SceneManager.LoadScene("Scene_Detection", LoadSceneMode.Single);
+    }
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
